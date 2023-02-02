fb16bd432217c67ae824de6c99fd780a1f6c83c9
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-02-03 00:16:43 +0530

    Simple input.java
    Given a list of numbers, stop processing input after the cumulative sum of all the input becomes negative.
    
    Input Format
    A list of integers to be processed
    
    Constraints
    All numbers input are integers between -1000 and 1000.
    
    Output Format
    Print all the numbers before the cumulative sum become negative.
    
    Sample Input
    1
    2
    88
    -100
    49
    Sample Output
    1
    2
    88

---
+++ Java Master Course/Fundamentals and patterns/Simple input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+		Scanner s= new Scanner(System.in);
+	int sum=0;
+	while(sum>=0){
+		int n=s.nextInt();
+		sum+=n;
+	
+		if(sum>=0)
+	System.out.println(n);
+	else
+	return ;
+		
+	}
+
+    }
+}
