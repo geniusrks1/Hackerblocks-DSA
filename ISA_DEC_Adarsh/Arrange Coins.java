10c93146ac5a98737b9830badf6a0bf7f7b24617
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-02-12 20:25:55 +0530

    Create Arrange Coins.java
    Aman is playing a game with coins. He has n coins in total.
    
    Aman wants to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
    
    Print the number of complete rows of the staircase Aman will build.
    
    Input Format
    Single Line consisting of total number of coins.
    
    Constraints
    1 <= N <= 231-1
    
    Output Format
    Print the number of complete rows of the staircase .
    
    Sample Input
    9
    Sample Output
    3 
    Explanation
    1st Row : 1 coin
    2nd Row : 2 coins
    3rd Row : 3 coins
    4th Row : 3 coins ( but 4th row is not completely filled upto its capacity )
    Hence total number of complete rows are 3.

---
+++ ISA_DEC_Adarsh/Arrange Coins.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main (String args[]) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int i;
+for(i=0;i*(i+1)/2<=n;i++){
+
+}
+System.out.print(i-1);
+    }
+}
