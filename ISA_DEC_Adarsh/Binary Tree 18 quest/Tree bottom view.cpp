3922208369ac9f4c2b98d022fd982a06cd669819
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-04-06 10:09:50 +0530

    Create Tree bottom view.cpp
    Given a binary tree , print the nodes in left to right manner as visible from below the tree
    
    Input Format
    Level order input for the binary tree will be given.
    
    Constraints
    No of nodes in the tree can be less than or equal to 10^7
    
    Output Format
    A single line containing space separated integers representing the bottom view of the tree
    
    Sample Input
    1 2 3 4 5 6 -1 -1 -1 -1 -1 -1 -1
    Sample Output
    4 2 6 3
    Explanation
    The tree looks like
    
                 1
              /      \
           2           3
        /     \       /
       4       5     6
    (Note that 5 and 6 are at the same position so we consider the right one to lower)

---
+++ ISA_DEC_Adarsh/Binary Tree 18 quest/Tree bottom view.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+	int data;
+	node* left, *right;
+
+	node(int data)
+	{
+		this->data=data;
+		this->left=NULL;
+		this->right=NULL;
+	}
+};
+
+node* buildLevelOrderBT()
+{
+	int d;
+	cin>>d;
+
+	if(d==-1)return NULL;
+	//tree exist
+
+	node* root=new node(d);
+	queue<node*>q;
+	q.push(root);
+
+	while(!q.empty())
+	{
+		node* curr=q.front();
+		q.pop();
+
+		int c1,c2;
+		cin>>c1>>c2;
+
+		if(c1!=-1)
+		{
+			//left child exist
+			curr->left=new node(c1);
+			q.push(curr->left);
+		}
+
+		if(c2!=-1)
+		{
+			//right child exist
+			curr->right=new node(c2);
+			q.push(curr->right);
+		}
+	}
+	return root;
+}
+
+
+
+void topView(node* root)
+{
+	if(!root)
+	{
+		cout<<"Tree is empty"<<endl;
+		return;
+	}
+
+	queue<pair<node*,int>>q; //queue<currnode,currLevel>
+	q.push({root,0});
+
+	map<int,int>mp; //map<level,topMostnode>
+
+	while(!q.empty())
+	{
+		auto temp=q.front();
+		q.pop();
+		node* currnode=temp.first;
+		int currLevel=temp.second;
+
+		if(!mp.count(currLevel))mp[currLevel]=currnode->data;
+
+		if(currnode->left)q.push({currnode->left,currLevel-1});
+		if(currnode->right)q.push({currnode->right,currLevel+1});
+	}
+
+	for(auto x:mp)
+	{
+		cout<<x.second<<" ";
+	}
+	cout<<endl;
+}
+
+void bottomView(node* root)
+{
+	queue<pair<node*, int>>q;
+	q.push({root,0});
+	map<int,int>mp;
+
+	while(!q.empty())
+	{
+		auto temp=q.front();
+		q.pop();
+		node* currnode=temp.first;
+		int level=temp.second;
+
+		mp[level]=currnode->data;
+
+		if(currnode->left)q.push({currnode->left,level-1});
+		if(currnode->right)q.push({currnode->right,level+1});
+	}
+
+	for(auto x:mp)cout<<x.second<<" ";
+	cout<<endl;
+
+
+}
+
+
+
+
+
+int main() {
+node* root=buildLevelOrderBT();
+bottomView(root);
+}
