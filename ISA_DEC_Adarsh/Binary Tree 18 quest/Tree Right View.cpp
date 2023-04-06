b1403d84600be029940ed7643f7eb8ab302ea4f2
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-04-06 10:06:40 +0530

    Create Tree Right View.cpp
    Given a binary tree , print its nodes from root to bottom as visible from right side of tree
    
    Input Format
    Level order input for the binary tree will be given.
    
    Constraints
    No of nodes in the tree can be less than or equal to 10^7
    
    Output Format
    A single line containing space separated integers representing the right view of the tree
    
    Sample Input
    1 2 3 4 5 -1 6 -1 -1 -1 -1 -1 -1
    Sample Output
    1 3 6
    Explanation
    The tree looks like
    
                 1
              /      \
           2           3
        /     \           \
       4       5           6
    When viewed from the left , we would see the nodes 1,3 and 6.

---
+++ ISA_DEC_Adarsh/Binary Tree 18 quest/Tree Right View.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+	int data;
+	node* left, *right;
+
+	node(int data)
+	{
+		this->data=data;
+		this->left=NULL;
+		this->right=NULL;
+	}
+};
+
+node* buildLevelOrderBT()
+{
+	int d;
+	cin>>d;
+
+	if(d==-1)return NULL;
+	//tree exist
+
+	node* root=new node(d);
+	queue<node*>q;
+	q.push(root);
+
+	while(!q.empty())
+	{
+		node* curr=q.front();
+		q.pop();
+
+		int c1,c2;
+		cin>>c1>>c2;
+
+		if(c1!=-1)
+		{
+			//left child exist
+			curr->left=new node(c1);
+			q.push(curr->left);
+		}
+
+		if(c2!=-1)
+		{
+			//right child exist
+			curr->right=new node(c2);
+			q.push(curr->right);
+		}
+	}
+	return root;
+}
+
+
+
+void rightViewHelper(node* root, int level, vector<int>&ans)
+{
+	//Base Case
+	if(!root)return;
+
+	//Recursive Case
+	if(level==ans.size())ans.push_back(root->data);
+
+	rightViewHelper(root->right,level+1,ans);
+	rightViewHelper(root->left,level+1,ans);
+}
+
+void rightView(node* root)
+{
+	vector<int>ans;
+	rightViewHelper(root,0,ans);
+	for(auto x:ans)cout<<x<<" ";
+	cout<<endl;
+}
+
+
+
+
+
+int main() {
+node* root=buildLevelOrderBT();
+rightView(root);
+}
