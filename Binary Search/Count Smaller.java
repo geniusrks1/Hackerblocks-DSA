c97eace7df8654282753148a3f96340582992502
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-04-19 19:40:28 +0530

    Update Count Smaller.java
    You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
    
    Input Format
    The first line of input will contain integer N, denoting the size of the array. Second line will contain N space-separated integers, denoting elements of the array.
    
    Constraints
    1 <= N <= 100000
    
    Output Format
    Print the output array in space-separated manner.
    
    Sample Input
    4
    5 2 6 1
    Sample Output
    2 1 1 0 
    Explanation
    To the right of 5 there are 2 smaller elements (2 and 1).
    To the right of 2 there is only 1 smaller element (1).
    To the right of 6 there is 1 smaller element (1).
    To the right of 1 there is 0 smaller element.

--- Binary Search/Count Smaller.java
+++ Binary Search/Count Smaller.java
@@ -1,1 +1,58 @@
 
+import java.util.*;
+public class Main {
+     public static List<Integer> countSmaller(int[] nums) {
+            
+int n=nums.length;
+ArrayList<Integer> list =new ArrayList<>();
+for(int i:nums)
+list.add(i);
+
+Collections.sort(list);
+
+ArrayList<Integer> ans =new ArrayList<>();
+        for(int i:nums){
+          int x=binaraySearchFirstIndex(list,i);
+          ans.add(x);
+          list.remove(x);
+        }
+return ans;
+    }
+
+
+
+    public static int binaraySearchFirstIndex(ArrayList<Integer> arr,int target){
+        int start=0;
+        int end=arr.size()-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr.get(mid)==target){
+            ans=mid;
+            end=mid-1;
+            }
+            else if(arr.get(mid)<target)
+            start=mid+1;
+            else
+            end=mid-1;
+        }
+return ans;
+    }
+     
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int v[] = new int[n];
+        for (int i =0;i<n;i++)
+        {
+            v[i] = sc.nextInt();
+        }
+        List<Integer> result = countSmaller(v);
+        for (int i : result)
+        {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
